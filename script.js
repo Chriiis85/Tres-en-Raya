542779741fbf288715034aa82672313f63aa46af
Christian Moreno Diaz <krizmoreno85@gmail.com>
2024-01-21 19:52:23 +0100

    a

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let tabla = document.querySelector("#tabla");
+let botones = document.querySelectorAll("button");
+
 let numerofilas = 3;
 let numerocolumnas = 3;
 let tablero = [];
@@ -25,15 +27,50 @@ function generarTablero() {
   }
 }
 
-function seleccionarCasilla(){
-    let posiciones = document.querySelectorAll("td");
-    for(let posicion of posiciones){
-        posicion.addEventListener("click", function(){
-            console.log(posicion);
-        })
-    }
+function seleccionarCasilla() {
+  let posiciones = document.querySelectorAll("td");
+  for (let posicion of posiciones) {
+    posicion.addEventListener("click", function () {
+      console.log(posicion);
+    });
+  }
 }
+function gestionarBotones() {
+  for (let boton of botones) {
+    boton.addEventListener("click", function () {
+      let botonseleccionado = boton.getAttribute("id");
+      switch (botonseleccionado) {
+        case "jugale":
+          console.log("Botón Jugador contra Aleatorio");
+          break;
+        case "jugia":
+          console.log("Botón Jugador contra IA");
+          break;
+        case "jugjug":
+          console.log("Botón Jugador contra Jugador");
+          break;
+        case "6fichas":
+          console.log("Botón 6 fichas");
+
+          break;
+        case "empezardenuevo":
+          console.log("Botón empezar de nuevo");
+
+          break;
+        case "9fichas":
+          console.log("Botón 9 fichas");
+
+          break;
+
+        default:
+          break;
+      }
+    });
+  }
+}
+
 pintarTablero();
 generarTablero();
 seleccionarCasilla();
+gestionarBotones();
 console.log(seleccionarCasilla());
