88bfc2f7e2007ecd7c01deceb6c79c731ada993e
Chriiis85 <krizmoreno85@gmail.com>
2024-01-25 19:54:26 +0100

    avance

--- script.js
+++ script.js
@@ -5,32 +5,32 @@ let textoturno = document.getElementById("turnojugador");
 var tipoPartida;
 
 /*CONTADOR DE PARTIDAS*/
-contPartidasGanadasJugador1 = 10;
-contPartidasPerdidasJugador1 = 10;
-contPartidasEmpatadasJugador1 = 10;
-contPartidasGanadasJugador2 = 1;
-contPartidasPerdidasJugador2 = 1;
-contPartidasEmpatadasJugador2 = 1;
+contPartidasGanadasJugador1 = 0;
+contPartidasPerdidasJugador1 = 0;
+contPartidasEmpatadasJugador1 = 0;
+contPartidasGanadasJugador2 = 0;
+contPartidasPerdidasJugador2 = 0;
+contPartidasEmpatadasJugador2 = 0;
 
 /*TD PARA CONTADOR DE PARTIDAS*/
 let partidasGanadasJugador1 = (document.getElementById(
   "partidas-ganadas-1"
-).innerHTML = contPartidasGanadasJugador1 + " Partida(s)");
+).innerHTML = " Partida(s)");
 let partidasPerdidasJugador1 = (document.getElementById(
   "partidas-perdidas-1"
-).innerHTML = contPartidasPerdidasJugador1 + " Partida(s)");
+).innerHTML = " Partida(s)");
 let partidasEmpatadasJugador1 = (document.getElementById(
   "partidas-empatadas-1"
-).innerHTML = contPartidasEmpatadasJugador1 + " Partida(s)");
+).innerHTML = " Partida(s)");
 let partidasGanadasJugador2 = (document.getElementById(
   "partidas-ganadas-2"
-).innerHTML = contPartidasGanadasJugador2 + " Partida(s)");
-let partidaPerdidasugador2 = (document.getElementById(
+).innerHTML = " Partida(s)");
+let partidaPerdidasJugador2 = (document.getElementById(
   "partidas-perdidas-2"
-).innerHTML = contPartidasPerdidasJugador2 + " Partida(s)");
+).innerHTML = " Partida(s)");
 let partidasEmpatadasJugador2 = (document.getElementById(
   "partidas-empatadas-2"
-).innerHTML = contPartidasEmpatadasJugador2 + " Partida(s)");
+).innerHTML = " Partida(s)");
 
 /*TABLRE Y FILAS*/
 let numerofilas = 3;
@@ -38,67 +38,14 @@ let numerocolumnas = 3;
 var tablero = [];
 
 /*TIEMPOS DE PARTIDA*/
-let tiempoPartida = 180;
+let contjug1 = document.getElementById("contador-1");
+let contjug2 = document.getElementById("contador-2");
+let contador;
 
 var turnoJugador1 = new Boolean(true);
+var hayCasillas = true;
 
-function gestionJuego() {
-  switch (tipoPartida) {
-    case "jugadorvsjugador":
-      pintarTablero();
-      generarTablero();
-      seleccionarCasilla();
-      textoturno.textContent = "Comienza la Partida, Turno del Jugador1.";
-      break;
-    case "jugadorvsaleatorio":
-      pintarTablero();
-      generarTablero();
-      seleccionarCasilla();
-      textoturno.textContent = "Comienza la Partida, Turno del Jugador1.";
-      break;
-    case "empezardenuevo":
-      location.reload();
-      break;
-    default:
-      break;
-  }
-}
-
-function hasGanado() {
-  setTimeout(() => {
-    alert("Ha ganado el jugador: " + turnoJugador());
-    location.reload();
-  }, "150");
-}
-function ganar() {
-  if (tablero[0][0] == 1 && tablero[0][1] == 1 && tablero[0][2] == 1) {
-    hasGanado();
-  }
-  if (tablero[1][0] == 1 && tablero[1][1] == 1 && tablero[1][2] == 1) {
-    hasGanado();
-  }
-  if (tablero[2][0] == 1 && tablero[2][1] == 1 && tablero[2][2] == 1) {
-    hasGanado();
-  }
-  if (tablero[0][0] == 2 && tablero[0][1] == 2 && tablero[0][2] == 2) {
-    hasGanado();
-  }
-  if (tablero[1][0] == 2 && tablero[1][1] == 2 && tablero[1][2] == 2) {
-    hasGanado();
-  }
-  if (tablero[2][0] == 2 && tablero[2][1] == 2 && tablero[2][2] == 2) {
-    hasGanado();
-  }
-}
-
-function reset() {
-  for (let i = 0; i < array.length; i++) {
-    for (let i = 0; i < array.length; i++) {
-      tablero[i][j] = 0;
-    }
-  }
-}
-
+/******************************************************************************************************************************************/
 /*GESTIONAR BOTONES*/
 function gestionarBotones() {
   for (let boton of botones) {
@@ -137,8 +84,92 @@ function gestionarBotones() {
   }
 }
 
+function gestionJuego() {
+  switch (tipoPartida) {
+    case "jugadorvsjugador":
+      pintarTablero();
+      generarTablero();
+      cronometro();
+      textoturno.textContent = "Comienza la Partida, Turno del Jugador1.";
+      break;
+    case "jugadorvsaleatorio":
+      pintarTablero();
+      generarTablero();
+      cronometro();
+      textoturno.textContent = "Comienza la Partida, Turno del Jugador1.";
+      break;
+    case "empezardenuevo":
+      location.reload();
+      break;
+    default:
+      break;
+  }
+}
+
+function hasGanado() {
+  alert("Ha ganado el jugador: " + turnoJugador());
+  reset();
+}
+
+function ganar() {
+  console.log(tablero);
+  /*HORIZONTAL*/
+  if (tablero[0][0] == 1 && tablero[0][1] == 1 && tablero[0][2] == 1) {
+    hasGanado();
+  } else if (tablero[1][0] == 1 && tablero[1][1] == 1 && tablero[1][2] == 1) {
+    hasGanado();
+  } else if (tablero[2][0] == 1 && tablero[2][1] == 1 && tablero[2][2] == 1) {
+    hasGanado();
+  } else if (tablero[0][0] == 2 && tablero[0][1] == 2 && tablero[0][2] == 2) {
+    hasGanado();
+  } else if (tablero[1][0] == 2 && tablero[1][1] == 2 && tablero[1][2] == 2) {
+    hasGanado();
+  } else if (tablero[2][0] == 2 && tablero[2][1] == 2 && tablero[2][2] == 2) {
+    hasGanado();
+  } else if (tablero[0][0] == 1 && tablero[1][0] == 1 && tablero[2][0] == 1) {
+    /*VERTICAL*/
+    hasGanado();
+  } else if (tablero[0][1] == 1 && tablero[1][1] == 1 && tablero[2][1] == 1) {
+    hasGanado();
+  } else if (tablero[0][2] == 1 && tablero[1][2] == 1 && tablero[2][2] == 1) {
+    hasGanado();
+  } else if (tablero[0][0] == 2 && tablero[1][0] == 2 && tablero[2][0] == 2) {
+    hasGanado();
+  } else if (tablero[0][1] == 2 && tablero[1][1] == 2 && tablero[2][1] == 2) {
+    hasGanado();
+  } else if (tablero[0][2] == 2 && tablero[1][2] == 2 && tablero[2][2] == 2) {
+    hasGanado();
+  } else if (tablero[0][0] == 1 && tablero[1][1] == 1 && tablero[2][2] == 1) {
+    hasGanado();
+  } else if (tablero[0][2] == 1 && tablero[1][1] == 1 && tablero[2][0] == 1) {
+    hasGanado();
+  } else if (tablero[0][0] == 2 && tablero[1][1] == 2 && tablero[2][2] == 2) {
+    hasGanado();
+  } else if (tablero[0][2] == 2 && tablero[1][1] == 2 && tablero[2][0] == 2) {
+    hasGanado();
+  }
+  if (!hayCasillas) {
+    alert("La partida termina en empate ");
+  }
+}
+function quedanCasillas() {
+  let cont = 0;
+  for (let i = 0; i < tablero.length; i++) {
+    for (let j = 0; j < tablero.length; j++) {
+      if (tablero[i][j] != 0) {
+        cont++;
+      }
+    }
+  }
+
+  if (cont == 9) {
+    hayCasillas = false;
+  }
+}
 /*PINTAR TABLERO ARRAY*/
 function pintarTablero() {
+  tabla.innerHTML = "";
+
   for (let i = 0; i < numerofilas; i++) {
     let nuevafila = document.createElement("tr");
     tabla.appendChild(nuevafila);
@@ -148,6 +179,7 @@ function pintarTablero() {
       nuevafila.appendChild(nuevaColumna);
     }
   }
+  seleccionarCasilla();
 }
 
 /*PINTAR TABLERO HTML*/
@@ -170,6 +202,7 @@ function seleccionarCasilla() {
     posicion.addEventListener("click", function () {
       let fila = posicion.parentNode.rowIndex;
       let columna = posicion.cellIndex;
+      quedanCasillas();
       gestionarCasilla(fila, columna);
       ganar();
     });
@@ -198,12 +231,15 @@ function generarAleatorio() {
     generarAleatorio();
   }
 }
+
 function gestionarCasilla(fila, columna) {
   if (tablero[fila][columna] == 0) {
     if (turnoJugador1) {
       tablero[fila][columna] = 1;
       turnoJugador1 = false;
       pintarCasilla(fila, columna, "blue");
+      clearInterval(contador);
+      cronometro();
       textoturno.textContent = "Turno del Jugador 2";
       if (tipoPartida == "jugadorvsaleatorio") {
         generarAleatorio();
@@ -212,6 +248,8 @@ function gestionarCasilla(fila, columna) {
       tablero[fila][columna] = 2;
       turnoJugador1 = true;
       pintarCasilla(fila, columna, "red");
+      clearInterval(contador);
+      cronometro();
       textoturno.textContent = "Turno del Jugador 1";
     }
   } else {
@@ -220,24 +258,10 @@ function gestionarCasilla(fila, columna) {
 }
 
 function pintarCasilla(fila, columna, color) {
-  let td = tabla.childNodes[fila + 3].childNodes[columna];
+  let td = tabla.children[fila].children[columna];
   td.style.backgroundColor = color;
 }
 
-/*CONTADOR TIEMPO RESTANTE JUGADA JUGADOR*/
-function contador() {
-  let segundosdisponiblepartida = 30;
-  let contador1 = (document.getElementById("contador-1").innerHTML =
-    "Tiempo restante jugada: " + segundosdisponiblepartida);
-  let contador2 = (document.getElementById("contador-2").innerHTML =
-    "Tiempo restante jugada: " + segundosdisponiblepartida);
-  if (segundosdisponiblepartida == 0) {
-    alert("Se ha llegado al fin del tiempo de partida del jugador 2");
-  } else {
-    segundosdisponiblepartida--;
-    setTimeout("contador()", 1000);
-  }
-}
 function turnoJugador() {
   if (turnoJugador1) {
     return "jugador1";
@@ -245,20 +269,34 @@ function turnoJugador() {
     return "jugador2";
   }
 }
-/*TEMPORIZADOR PARTIDA TOTAL*/
-function temporizadorPartida() {
-  let tiempo = (document.getElementById("tiempo-partida").innerHTML =
-    "Tiempo restante jugada: " + tiempoPartida);
-  if (tiempoPartida == 0) {
-    alert("Se ha consumido el tiempo de patida");
-  } else {
-    tiempoPartida--;
-    setTimeout("temporizadorPartida()", 1000);
-  }
+function reset() {
+  pintarTablero();
+  generarTablero();
+  textoturno.textContent = "Comienza la Partida, Turno del Jugador1.";
+  clearInterval(contador);
+  clearInterval(contador);
+  contjug1.innerHTML = "Tiempo Restante: 0";
+  contjug2.innerHTML = "Tiempo Restante: 0";
 }
 
-//contador();
-//temporizadorPartida();
-/*Esencial para arrancar*/
+function cronometro() {
+  let segundos = 30;
+
+  contador = setInterval(function () {
+    if (segundos == 0) {
+      textoturno.textContent = "Se te acabo el tiempo: " + turnoJugador();
+      textoturno.style.color = "red";
+    } else {
+      segundos--;
+      if (turnoJugador1) {
+        contjug1.innerHTML = "Tiempo restante: " + segundos;
+        contjug2.innerHTML = "Tiempo Restante: 0";
+      } else {
+        contjug2.innerHTML = "Tiempo restante: " + segundos;
+        contjug1.innerHTML = "Tiempo Restante: 0";
+      }
+    }
+  }, 1000);
+}
+
 gestionarBotones();
-//reset();
